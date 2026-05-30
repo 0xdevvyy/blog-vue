41211eed955924e03cfb504e7357036f38f99029
0xdevvyy <pullhead45@gmail.com>
2026-05-30 12:52:49 +0800

    feat(landing): successfullyt added a landing page

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- <p class="uppercase tracking-[0.2em] text-xs mb-5 text-zinc-700">
+      Software Engineering , Isolation , Debugging at 2AM
+    </p> -->
+
+    <h1 class="text-4xl md:text-6xl leading-none font-black uppercase mb-6">
+      Building things
+      <br />
+      while slowly losing my mind.
+    </h1>
+
+    <div class="w-f h-0.75 bg-black mb-8"></div>
+
+    <p class="text-xl leading-relaxed text-zinc-800 max-w-xl">
+      A Personal Blog of an Information Technology Graduate documenting projects, technical
+      learnings, and the journey toward a career in software development. After one year of
+      unemployment, I figured I might as well document the things that i build, and leave evidence
+      that I existed on the internet.
+    </p>
+
+    <div class="flex gap-4 mt-10">
+      <button
+        class="border-2 border-black px-8 py-3 uppercase tracking-[0.2em] text-sm hover:bg-black hover:text-[#e7e2d8] transition"
+      >
+        Read Journal
+      </button>
+
+      <button class="uppercase tracking-[0.2em] text-sm hover:underline underline-offset-4">
+        Archive
+      </button>
+      <button class="uppercase tracking-[0.2em] text-sm hover:underline underline-offset-4">
+        Blog
+      </button>
+    </div>
+  </div>
+</template>
